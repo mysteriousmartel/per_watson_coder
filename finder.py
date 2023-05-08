47306b69a32b95782976c9401969fcd09cd3550a
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2023-05-08 14:34:40 -0500

    Add files via upload

---
+++ finder.py
@@ -0,0 +1,17 @@
+import json
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+authenticator = IAMAuthenticator('')#Insert API key here
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2021-03-25',
+    authenticator=authenticator)
+
+natural_language_understanding.set_service_url('')# Insert URL here
+
+model = natural_language_understanding.get_classifications_model(
+  model_id='',#Insert model_id
+).get_result()
+
+
+print(json.dumps(model, indent=2))
